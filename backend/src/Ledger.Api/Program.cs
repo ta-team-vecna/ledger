8cab4af745dc0bf60abad9bb47ed0f511331ecb4
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-11 17:04:01 +0200

    feat: prepare db for work

--- backend/src/Ledger.Api/Program.cs
+++ backend/src/Ledger.Api/Program.cs
@@ -1,22 +1,17 @@
+using Ledger.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AppDbContext>(options => { options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); });
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
