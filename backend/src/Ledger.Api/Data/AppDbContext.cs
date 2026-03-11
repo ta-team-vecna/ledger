c1c62374d90604165e5035c5af7945d96859d844
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-12 00:40:55 +0200

    feat: modelize user & create migrations

--- backend/src/Ledger.Api/Data/AppDbContext.cs
+++ backend/src/Ledger.Api/Data/AppDbContext.cs
@@ -5,14 +5,36 @@ namespace Ledger.Api.Data;
 
 public sealed class AppDbContext : DbContext {
     public DbSet<ApplicationUser> Users => Set<ApplicationUser>();
-    public DbSet<Equipment> Equipment => Set<Equipment>();
-    public DbSet<EquipmentRequest> EquipmentRequests => Set<EquipmentRequest>();
+    // public DbSet<Equipment> Equipment => Set<Equipment>();
+    // public DbSet<EquipmentRequest> EquipmentRequests => Set<EquipmentRequest>();
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
-        
-        // TODO: implement entities using fluid api
+
+        // TODO: finish implementing entities using fluid api
+
+        modelBuilder.Entity<ApplicationUser>(entity => {
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            entity.Property(x => x.PasswordHash)
+                .IsRequired();
+
+            entity.HasIndex(x => x.Email)
+                .IsUnique();
+        });
     }
 }
