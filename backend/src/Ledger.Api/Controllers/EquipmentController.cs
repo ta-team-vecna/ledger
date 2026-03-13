bfc4455ced934cb0910ec10cd7359013f5cd284c
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-13 22:41:15 +0200

    feat: equipment controller stub

---
+++ backend/src/Ledger.Api/Controllers/EquipmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ledger.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public sealed class EquipmentController : ControllerBase {
+    
+}
