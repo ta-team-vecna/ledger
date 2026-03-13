efc7214642f9b8b1f3db823928c78f9972fb526f
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-13 22:41:56 +0200

    feat: db connection to equipment

--- backend/src/Ledger.Api/Controllers/EquipmentController.cs
+++ backend/src/Ledger.Api/Controllers/EquipmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Ledger.Api.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ledger.Api.Controllers;
@@ -7,5 +8,9 @@ namespace Ledger.Api.Controllers;
 [Route("api/[controller]")]
 [Authorize]
 public sealed class EquipmentController : ControllerBase {
-    
+    private readonly AppDbContext _db;
+
+    public EquipmentController(AppDbContext db) {
+        _db = db;
+    }
 }
