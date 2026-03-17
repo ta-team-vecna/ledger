c846b04cb7a719c87cb32f7526b0c3657b1ce948
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-17 16:48:01 +0200

    feat: refresh token model

---
+++ backend/src/Ledger.Api/Migrations/20260317144535_RenameRefreshTokenExpiry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ledger.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameRefreshTokenExpiry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RefreshTokenExpiryDate",
+                table: "Users",
+                newName: "RefreshTokenExpiryTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                newName: "RefreshTokenExpiryDate");
+        }
+    }
+}
